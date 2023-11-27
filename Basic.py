3af985f72187643d40f709fb8c6c15b3b246e9fc
Priaynk911 <panchalpriyank48@gmail.com>
2023-11-27 12:48:51 +0530

    ADD CODE

---
+++ Basic.py
@@ -0,0 +1,40 @@
+# Print First Statement
+
+#print("Hi..Developer_Priyank")
+ 
+# Storing Some Value in Variables
+
+# name= "priyank" # String Type
+# print(name)
+# age= 18  # Integer type
+# print(age)
+# is_adult=True # boolean type 
+# print(is_adult)
+
+# User input Program 
+
+# Name= input("what Your Name:- ")
+# print("Have A Great Day " + Name)
+
+# Making Interesting Program using Input
+
+# Current_Age= input("Enter Your Age:- ")
+# Old_Age = int(Current_Age) - 2 
+# print(Old_Age)
+
+# Take Two Input and Print The Sum of Two Number
+
+# Firstno=input("Enter The Number One:- ")
+# Secondno=input("Enter The Number Two:- ")
+# sum = int(Firstno)+int(Secondno)
+# print("Sum of Two Number is:-" + str(sum))
+
+# String----> Function
+
+#Upper_Case ---> Here you Get all Upper Case of All character
+name = "Developer_Priyank"
+print(name.upper())
+#Lower_Case ---> Here you Get all Lower Case of All Character
+print(name.lower())
+#find_case ---> Here you Get 
+print(name.find('l'))
