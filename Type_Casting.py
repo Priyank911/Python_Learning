c85c0ae272c3e8efa84d4c86334594583220ff92
Priaynk911 <panchalpriyank48@gmail.com>
2023-12-06 21:33:44 +0530

    Added TypeCasting File

---
+++ Type_Casting.py
@@ -0,0 +1,13 @@
+#TypeCasting = Convert one datatype to another datatype
+x = 1
+y = 2.0
+z = "3"
+
+y = int(y) # method 1
+print(int(y)) # method 2
+print(type(y)) # here we check the type of method
+
+#z = int(z)
+print(z*3) # Check the output after and before the above code
+
+#Noted:- TypeError -->(String are only concatenate with String)
