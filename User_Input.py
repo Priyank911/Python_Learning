fc47f9f53727bbd1d7cb895d9e45c3d6da3041dc
Priaynk911 <panchalpriyank48@gmail.com>
2023-12-06 22:31:50 +0530

    Added User_Input file

---
+++ User_Input.py
@@ -0,0 +1,18 @@
+# name = input("What is Your name:- ")
+# Age = int(input("What is your Age:- "))
+# Height = float(input("How tall you are:- "))
+
+# Age = Age + 1
+
+# print("Hello "+ name)
+# print("Your Age is",Age)
+# print("Your Height is",Height)
+
+# Fun with User_Input
+
+user_Name = input("What is Your name:- ")
+if user_Name :
+    name = user_Name
+else:
+    name = 'N/A'
+print(name)
