391c158bb5763da10d33f0ad922590f5f753afa2
Priaynk911 <panchalpriyank48@gmail.com>
2023-12-07 22:32:15 +0530

    Added String_Slicing file

---
+++ String_Slicing.py
@@ -0,0 +1,23 @@
+# Method for Slicing:- [start:stop:step]
+
+# name = "Panchal Priyank"
+
+# first_name=name[:7]
+# last_name=name[8:15]
+# funky_name=name[0:15:2]
+# reversed_name = name[::-1]
+
+# print(first_name)
+# print(last_name)
+# print(funky_name)
+# print(reversed_name)
+
+#Important
+
+Website1= "https://google.com"
+Website2= "https://wikipedia.com"
+
+slice = slice(8,-4)
+
+print(Website1[slice])
+print(Website2[slice])
