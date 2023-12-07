e01d91c9c3bf4abb845e1c45e04c09c0ac13b48a
Priaynk911 <panchalpriyank48@gmail.com>
2023-12-07 21:55:57 +0530

    Added Maths_Function file

---
+++ maths_function.py
@@ -0,0 +1,16 @@
+# First off all we import the library of maths
+import math
+
+pi =3.14
+x=10
+y=11
+z=7
+
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(pow(pi,2))
+# print(math.sqrt(pi))
+print(min(x,y,z))
+print(max(x,y,z))
