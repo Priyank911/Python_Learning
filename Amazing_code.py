0e7f3a4b03c5b0cf592b0bcdc2bfe4e282ed76d6
Priaynk911 <panchalpriyank48@gmail.com>
2023-12-08 21:55:41 +0530

    Added if statment file

--- Amazing_code.py
+++ Amazing_code.py
@@ -1,5 +1,10 @@
-var: str = 'Python'
+# var: str = 'Python'
 
-print(f'{var:_>20}')
-print(f'{var:_<20}')
-print(f'{var:~^20}')
+# print(f'{var:_>20}')
+# print(f'{var:_<20}')
+# print(f'{var:~^20}')
+
+# Python Pistol Operator
+
+*_, = 'abcd'
+print(_)
---
+++ if_statments.py
@@ -0,0 +1,10 @@
+age = int(input("How Old are You:- "))
+
+if age == 100:
+    print("Century ho gai🧓")
+elif age >= 18:
+    print("You are an adult")
+elif age < 0:
+    print("You haven't been born🤣")
+else:
+    print("You are a child!")
