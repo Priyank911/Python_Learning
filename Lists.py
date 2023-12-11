4697b7556708bb2d31d380cd7c92f3f94f8df567
Priaynk911 <panchalpriyank48@gmail.com>
2023-12-11 23:14:51 +0530

    Added List File

---
+++ Lists.py
@@ -0,0 +1,15 @@
+#list is used to store multiple value in one variables
+
+Food =["Cake","Pizza","Pasta","Burger","Frankie","FrenchFries"]
+
+Food[0] = "Dosa"
+# Some Function in Lists:- 
+
+Food.append("Ice Cream")
+Food.remove("Pasta")
+Food.pop()
+Food.insert(0,"Cake")
+
+# Food.clear()
+
+print(Food)
