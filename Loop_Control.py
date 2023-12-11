79b54e8e18ca3baff8e9e7cce39180f09fb4a939
Priaynk911 <panchalpriyank48@gmail.com>
2023-12-11 22:58:16 +0530

    Added Loop Control Code

--- Loop_Control.py
+++ Loop_Control.py
@@ -5,3 +5,20 @@
 # Continue = skips to the loop entirely 
 # pass = does nothing, acts as a placeholder
 
+# while True:
+#     name = input("Enter the your name: ")
+#     if name != "":
+#         break
+
+# phone_number = "123-456-789"
+
+# for i in  phone_number:
+#     if i == "-":
+#         continue
+#     print(i, end="")
+
+for i in range(1,21):
+    if i==13:
+        pass
+    else:
+      print(i)
