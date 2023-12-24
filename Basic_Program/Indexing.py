173b13ecece2512937ec743eeab5639ef01f58a6
Priaynk911 <panchalpriyank48@gmail.com>
2023-12-24 20:12:44 +0530

    Added Index File

---
+++ Basic_Program/Indexing.py
@@ -0,0 +1,10 @@
+# Index operator [] = Gives access to a sequence's element (str, list, tuples)
+
+name= "priyank"
+
+# if(name[0].islower()):
+#     name = name.capitalize()
+# print(name)
+
+first_name = name[0:3].upper()
+print(first_name)
