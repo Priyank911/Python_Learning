783d4a1b0b954e678e8b4d7dd58c37f015cd424a
Priaynk911 <panchalpriyank48@gmail.com>
2024-01-09 21:51:29 +0530

    Added Some Program

---
+++ Basic_Program/Static_variables.py
@@ -0,0 +1,12 @@
+class Cal:
+    a=20
+    b=30
+    def add(self,a,b):
+        self.a=Cal.a
+        self.b=Cal.b
+        c=self.a+self.b
+        print(c)
+call = Cal()
+call.add(Cal.a,Cal.b)
+
+# Program Done by OC ( Noted )
