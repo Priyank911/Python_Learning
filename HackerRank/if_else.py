2ed7812028dfbaa26c00788827cd67ec0669d5da
Priaynk911 <panchalpriyank48@gmail.com>
2024-02-15 23:19:13 +0530

    New

--- HackerRank/if_else.py
+++ HackerRank/if_else.py
@@ -4,6 +4,12 @@ import random
 import re
 import sys
 
-num = input("Enter the number:- ")
-if(num/2==1):
+num = int(input("Enter the Number:- "))
+if(num%2==1):
     print("Weird")
+elif num % 2 == 0 and 2 <= num <= 5:
+    print("Not Weird")
+elif num % 2 == 0 and 6 <= num <= 20:
+    print("Weird")
+elif num % 2 == 0 and num > 20:
+    print("Not Weird")
